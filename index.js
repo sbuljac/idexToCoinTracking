d09118263f90613d8257e2f723568397c74bbb9a
sbuljac <sbuljac@gmail.com>
2018-03-07 12:21:14 +0100

    Add eslintrc and standard package

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const fs = require('fs')
 const argv = require('optimist').argv
 const csv = require('csv')
 
-fs.createReadStream(argv._[0]).
-  pipe(csv.parse({columns: true, relax_column_count: true, delimiter: '\t'})).
-  pipe(csv.transform((record) => {
+fs.createReadStream(argv._[0])
+  .pipe(csv.parse({ columns: true, relax_column_count: true, delimiter: '\t' }))
+  .pipe(csv.transform((record) => {
     const [Buy, CurB] = record.Amount.split(' ')
     const [Total, CurS] = record.Total.split(' ')
     const [Fee, CurF] = record.Fee.split(' ')
@@ -21,8 +21,8 @@ fs.createReadStream(argv._[0]).
       Group: '',
       Comment: '',
       'Trade ID': record['Transaction ID'],
-      Date: record.Date,
+      Date: record.Date
     }
-  })).
-  pipe(csv.stringify({header: true})).
-  pipe(fs.createWriteStream(`${argv._[0].split('.')[0]}FormatedForCoinTracking.csv`))
+  }))
+  .pipe(csv.stringify({ header: true }))
+  .pipe(fs.createWriteStream(`${argv._[0].split('.')[0]}FormatedForCoinTracking.csv`))
